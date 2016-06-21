d8c99ea50c7d14078ee0734dcc148ce501ab7322
Lucas <lucas.servare@gmail.com>
2016-06-20 22:04:05 -0400

    Allow user to select how far back they want to look on the recent page.

--- public/javascripts/recent_earthquakes.js
+++ public/javascripts/recent_earthquakes.js
@@ -1,5 +1,18 @@
-function earthquakesInLastHour() {
-  var url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
+function earthquakesInLastHour(choice) {
+  var url;
+
+  switch(choice) {
+    case 'hour':
+      url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
+      break;
+    case 'day':
+      url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+      break;
+    case 'week':
+      url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+      break;
+  }
+
   $.get(url, function(data) {
     for (i=0; i < data.features.length; i++){
       $("#eq-last-hour").append("<br>Magnitude " + data.features[i].properties.mag + " located " + data.features[i].properties.place + "." + "<br>");
@@ -7,4 +20,9 @@ function earthquakesInLastHour() {
   });
 }
 
-earthquakesInLastHour();
+$('#selector').change(function() {
+  var choice = $('#selector').val();
+  earthquakesInLastHour(choice)
+});
+
+earthquakesInLastHour('hour');
