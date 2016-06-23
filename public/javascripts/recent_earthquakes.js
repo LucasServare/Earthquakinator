a6ad34444a5df26b06369270ea0954cb4f6ea348
Lucas <lucas.servare@gmail.com>
2016-06-22 22:32:20 -0400

    Make recent earthquakes page look nicer.

--- public/javascripts/recent_earthquakes.js
+++ public/javascripts/recent_earthquakes.js
@@ -1,6 +1,5 @@
 function earthquakesInLastHour(choice) {
   var url;
-
   switch(choice) {
     case 'hour':
       url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
@@ -12,18 +11,17 @@ function earthquakesInLastHour(choice) {
       url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
       break;
   }
-
   $.get(url, function(data) {
+    $('#eq-last').empty();
     for (i=0; i < data.features.length; i++){
-      $("#eq-last-hour").append("<br>Magnitude " + data.features[i].properties.mag + " located " + data.features[i].properties.place + "." + "<br>");
+      $("#eq-last").append("<br>Magnitude " + data.features[i].properties.mag + " located " + data.features[i].properties.place + "." + "<br>");
     }
   });
 }
 
-$('#selector').change(function() {
-  var choice = $('#selector').val();
-  $("#type").html(choice);
+$('input[name=time]').change(function() {
+  var choice = $('input[name=time]:checked').val()
   earthquakesInLastHour(choice)
-});
+})
 
 earthquakesInLastHour('hour');
