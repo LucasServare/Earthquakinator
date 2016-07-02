45557b125632422750e20483241bb6939995794c
Lucas <lucas.servare@gmail.com>
2016-07-02 11:45:26 -0400

    Appropriately use routing in Express.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/maps', function(req, res, next) {
+  res.render('maps');
+});
+
+router.get('/recent', function(req, res, next) {
+  res.render('recent');
+})
 
 module.exports = router;
