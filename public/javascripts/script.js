d031d80856b70dff00b006ad5648559c78c87ddc
Lucas <lucas.servare@gmail.com>
2016-07-14 20:37:52 -0400

    Fix situations where less than 10 earthquakes were returned. Also, don't limit ourselves to only graphing 10 earthquakes. Graph them all! The world is your oystergit add -A

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -230,77 +230,17 @@ $('#map-container').html('')
       }
   });
 
-  var earthquakes = [{
-    name: eqObjects[0].name,
-    latitude: eqObjects[0].latitude,
-    longitude: eqObjects[0].longitude,
-    radius: Math.pow(eqObjects[0].mag/5, 5) * 5,
-    magnitude: eqObjects[0].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[1].name,
-    latitude: eqObjects[1].latitude,
-    longitude: eqObjects[1].longitude,
-    radius: Math.pow(eqObjects[1].mag/5, 5) * 5,
-    magnitude: eqObjects[1].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[2].name,
-    latitude: eqObjects[2].latitude,
-    longitude: eqObjects[2].longitude,
-    radius: Math.pow(eqObjects[2].mag/5, 5) * 5,
-    magnitude: eqObjects[2].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[3].name,
-    latitude: eqObjects[3].latitude,
-    longitude: eqObjects[3].longitude,
-    radius: Math.pow(eqObjects[3].mag/5, 5) * 5,
-    magnitude: eqObjects[3].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[4].name,
-    latitude: eqObjects[4].latitude,
-    longitude: eqObjects[4].longitude,
-    radius: Math.pow(eqObjects[4].mag/5, 5) * 5,
-    magnitude: eqObjects[4].mag,
-    fillKey: 'earthquakes'
-  },{
-  name: eqObjects[5].name,
-  latitude: eqObjects[5].latitude,
-  longitude: eqObjects[5].longitude,
-  radius: Math.pow(eqObjects[5].mag/5, 5) * 5,
-  magnitude: eqObjects[5].mag,
-  fillKey: 'earthquakes'
-  },{
-    name: eqObjects[6].name,
-    latitude: eqObjects[6].latitude,
-    longitude: eqObjects[6].longitude,
-    radius: Math.pow(eqObjects[6].mag/5, 5) * 5,
-    magnitude: eqObjects[6].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[7].name,
-    latitude: eqObjects[7].latitude,
-    longitude: eqObjects[7].longitude,
-    radius: Math.pow(eqObjects[7].mag/5, 5) * 5,
-    magnitude: eqObjects[7].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[8].name,
-    latitude: eqObjects[8].latitude,
-    longitude: eqObjects[8].longitude,
-    radius: Math.pow(eqObjects[8].mag/5, 5) * 5,
-    magnitude: eqObjects[8].mag,
-    fillKey: 'earthquakes'
-  },{
-    name: eqObjects[9].name,
-    latitude: eqObjects[9].latitude,
-    longitude: eqObjects[9].longitude,
-    radius: Math.pow(eqObjects[9].mag/5, 5) * 5,
-    magnitude: eqObjects[9].mag,
-    fillKey: 'earthquakes'
-  }];
+  var earthquakes = []
+  for (i=0; i < eqObjects.length; i++) {
+    earthquakes.push({
+      name: eqObjects[i].name,
+      latitude: eqObjects[i].latitude,
+      longitude: eqObjects[i].longitude,
+      radius: Math.pow(eqObjects[i].mag/5, 5) * 10,
+      magnitude: eqObjects[i].mag,
+      fillKey: 'earthquakes'
+    })
+  }
 
   //Draw earthquakes on map
   map.bubbles(earthquakes, {
