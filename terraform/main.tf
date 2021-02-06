ebd57a51b35732c467a83b64dbd9e8d0aeaa7359
Lucas <lucas.servare@gmail.com>
2021-02-06 15:45:12 -0500

    Adding the docker credential helper to our startup script.

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,8 +37,18 @@ resource "google_compute_instance" "earthquakinator" {
   }
 
   metadata = {
-    # gce-container-declaration = "us-east4-docker.pkg.dev/earthquakinator/earthquakinator-images/earthquakinator-image:latest"
-    startup-script = "docker run us-east4-docker.pkg.dev/earthquakinator/earthquakinator-images/earthquakinator-image:latest"
+    startup-script = "
+      VERSION=2.0.0
+      OS=linux
+      ARCH=amd64
+
+      curl -fsSL 'https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz'
+      | tar xz --to-stdout ./docker-credential-gcr \
+      > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr
+
+      docker-credential-gcr configure-docker --registries=us-east4-docker.pkg.dev
+
+      docker run us-east4-docker.pkg.dev/earthquakinator/earthquakinator-images/earthquakinator-image:latest"
   }
 }
 
